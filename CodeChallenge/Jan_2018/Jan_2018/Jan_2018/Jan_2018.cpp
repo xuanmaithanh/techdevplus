dc55216ebca0c3abbaed391e78f8454f3b04c774
xuanmaithanh <31360157+xuanmaithanh@users.noreply.github.com>
2018-01-10 06:25:28 +0700

    - Update for Code Challenges (10/1/2018), added the solution for Code question

--- CodeChallenge/Jan_2018/Jan_2018/Jan_2018/Jan_2018.cpp
+++ CodeChallenge/Jan_2018/Jan_2018/Jan_2018/Jan_2018.cpp
@@ -5,45 +5,55 @@
 #include <iostream>
 using namespace std;
 
-bool isPrime(int _number)
+bool isPrimeNumber(int _number)
 {
 	if (_number < 2) return false;
-	for (int j = _number / 2; j >= 2; j--)
+
+	if (_number == 2) return true;
+
+	if (_number % 2 == 0) return false;
+
+	int n_toCheck = (_number / 3) + 1;
+	for (int j = 3; j <= n_toCheck; j = j + 2)
 	{
 		if (_number % j == 0) return false;
+		else n_toCheck = (_number / j) + 1;
 	}
 	return true;
 }
 
-void main()
+int main()
 {
-	bool isRunning = true;
-	while (isRunning)
+	int N;
+	cout << "Input an Integer (>= 0): \n";
+	cin >> N;
+	if (N < 0) return -1;
+
+	int i = N / 2;
+	int countLoop = 1;
+
+	for (; i >= 2; countLoop++)
 	{
-		unsigned int N;
-		cout << "Input an Integer (>= 0): \n";
-		cin >> N;
-		if (N < 0) isRunning = false;
-		int i = N / 2;
-		for (; i >= 2; i--)
-		{
-			if ((N % i == 0) & isPrime(i)) break;
-		}
-		if (i > 1)
-		{
-			cout << i;
-		}
-		else if (N >= 2)
-		{
-			cout << N;
-		}
-		else
-		{
-			cout << "-1";
-		}
+		if ((N % i == 0) && isPrimeNumber(i)) break;
+		i = N / (countLoop + 1);
 	}
+
+	if (i > 1)
+	{
+		cout << i;
+	}
+	else if (N >= 2)
+	{
+		cout << N;
+	}
+	else
+	{
+		cout << "-1";
+	}
+
 	cout << "\n";
 	system("pause");
+	return 0;
 }
 
 
